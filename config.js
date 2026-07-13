66a5e42d32acd441f213798e2b22dd73c1121f80
albertoquintanainf <alberto.quintanainf@gmail.com>
2026-07-13 14:21:08 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 // Si este archivo no existe, el dashboard funciona en modo estático
 // (solo lectura, sin edición).
 // -------------------------------------------------------------
-window.SUPABASE_URL      = "https://zwrtkmzrgkpvpfeofmhu.supabase.co/rest/v1";
+window.SUPABASE_URL      = "https://zwrtkmzrgkpvpfeofmhu.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_zX99mgJ18gzsGaToiVSK2w_4fIBOH7h";
 window.SHARED_PASSWORD   = "hece2026";
 
